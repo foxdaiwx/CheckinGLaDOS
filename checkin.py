09501f47314dbb3d1dea5d2b72bb8df6044a0f81
Fox <foxdai@163.com>
2025-03-17 18:25:44 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -18,7 +18,7 @@ def check_in():
         'cookie': COOKIE,
         'origin': origin,
         'referer': referer,
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
         'content-type': 'application/json'
     }
 
@@ -26,11 +26,17 @@ def check_in():
         response = requests.post(url, headers=headers, data=json.dumps(payload))
         result = response.json()
         
-        if result['code'] == 0:
+        #Code=0,表示代码运行成功，然后获取关联信息并显示
+        if result['code'] == 0:     
+            #获取代码运行结果的信息
             message = result['message']
+            #获取天数
             days = result['list'][0]['balance']
+            #回复信息和天数
             return f"{message} - 剩余天数: {days}"
+        #否则，code<>0，则显示出错信息，比如Invalid Token，可能是Cookie过期货不对
         return f"签到失败: {result.get('message', '未知错误')}"
+    #其他情况，比如代码出错、网络问题等，比如connection error；
     except Exception as e:
         return f"请求异常: {str(e)}"
 
