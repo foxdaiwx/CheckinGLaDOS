163a9912547cfac23b2b3efe960079c431a41506
Fox <foxdai@163.com>
2025-03-19 23:20:46 +0800

    about result text file where to save

--- checkin.py
+++ checkin.py
@@ -32,8 +32,9 @@ def check_in():
             message = result['message']
             #获取积分总点数
             points = result['list'][0]['balance']
+            userid = result['list'][0]['user_id']
             #回复前面获取的信息和各个值
-            return f"{message} - 总积分: {points}"
+            return f"{message} - user id: {userid} - 总积分: {points}"
         #否则，code<>0，则显示出错信息，比如Invalid Token，可能是Cookie过期或不对
         return f"签到失败: {result.get('message', '未知错误')}"
     #其他情况，比如代码出错、网络问题等，比如connection error；
